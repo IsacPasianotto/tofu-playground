dc7b879e5312587b9b6b61a3cce267c0d8a3a0aa
IsacPasianotto <yh4mop12z@gmail.com>
2025-06-16 09:40:04 +0200

    added prxmox variables

--- variables.tf
+++ variables.tf
@@ -20,3 +20,46 @@ variable "ssh_key" {
   description = "SSH public key to use for VM access, required."
   type = string
 }
+
+#
+# Proxmox specific variables
+#
+
+variable "proxmox_endpoint" {
+  type = string
+  description = "Proxmox API endpoint, e.g., 'https://proxmox.example.com:8006/"
+}
+
+variable "proxmox_api_token" {
+  type = string
+  description = "Proxmox API token in the format '<user@realm>!<token_name>=<token_value>'"
+}
+
+variable "proxmox_ssh_agent_username" {
+  type = string
+  description = "Username for SSH agent authentication in Proxmox"
+  default = "root"
+}
+
+variable "proxmox_target_node_name" {
+  type = string
+  description = "Proxmox target node name where VMs will be created"
+  default = "pve01"
+}
+
+variable "proxmox_template_id" {
+  type = number
+  description = "Proxmox template ID cloned during VM creation"
+}
+
+variable "proxmox_VM_id_start" {
+  type = number
+  description = "Starting VM ID for Proxmox VMs"
+  default = 100
+}
+
+variable "proxmox_vm_tag" {
+  type = string
+  description = "Tag to apply to Proxmox VMs"
+  default = "tofu"
+}
